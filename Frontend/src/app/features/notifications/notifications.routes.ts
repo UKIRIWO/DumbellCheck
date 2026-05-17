910486bae279056aa529bafd4c9c07b86c490a23
UKIRIWO <adriangogu1508@gmail.com>
2026-05-17 19:36:59 +0200

    componentes olvidados

--- Frontend/src/app/features/notifications/notifications.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const NOTIFICATIONS_ROUTES: Routes = [
-  {
-    path: '',
-    loadComponent: () =>
-      import('./pages/notifications-page/notifications-page.component').then((m) => m.NotificationsPageComponent),
-  },
-];
